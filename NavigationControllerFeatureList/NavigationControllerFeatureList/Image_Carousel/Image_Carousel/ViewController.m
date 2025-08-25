08327609a3af74165079b962b2ff3e4f17ccae50
xianglong <xianglong@bytedance.com>
2025-08-25 20:36:33 +0800

    feat: 实现无限图片轮播功能

--- NavigationControllerFeatureList/NavigationControllerFeatureList/Image_Carousel/Image_Carousel/ViewController.m
+++ NavigationControllerFeatureList/NavigationControllerFeatureList/Image_Carousel/Image_Carousel/ViewController.m
@@ -15,7 +15,13 @@
 @interface ImageCarouselViewController () <UIScrollViewDelegate>
 @property(nonatomic, strong) UIScrollView* scrollView;
 @property(nonatomic, strong) UIPageControl* pageControl;
+
+@property(nonatomic, strong) UIImageView* leftImageView;
+@property(nonatomic, strong) UIImageView* centerImageView;
+@property(nonatomic, strong) UIImageView* rightImageView;
+
 @property(nonatomic, strong) NSArray<UIImage*>* images;
+@property(nonatomic, assign) NSInteger currentIndex;
 @end
 
 @implementation ImageCarouselViewController
@@ -27,6 +33,7 @@
     [self loadData];
     [self setupScrollView];
     [self setupPageControl];
+    [self updateImageViews];
 }
 
 
@@ -51,18 +58,43 @@
     self.scrollView.showsVerticalScrollIndicator = NO;
     self.scrollView.showsHorizontalScrollIndicator = NO;
     
-    CGFloat contentWidth = SCREEN_WIDTH * IMAGE_COUNT;
-    self.scrollView.contentSize = CGSizeMake(contentWidth, SCROLL_VIEW_H - 1);
+//    CGFloat contentWidth = SCREEN_WIDTH * IMAGE_COUNT;
+//    self.scrollView.contentSize = CGSizeMake(contentWidth, SCROLL_VIEW_H - 1);
+    self.scrollView.contentSize = CGSizeMake(SCREEN_WIDTH * 3, SCROLL_VIEW_H - 1);
+    [self.scrollView setContentOffset:CGPointMake(SCREEN_WIDTH, 0) animated:NO];
+    [self.view addSubview:self.scrollView];
     
-    for (int i = 0; i < IMAGE_COUNT; ++i) {
-        UIImageView* imageView = [[UIImageView alloc] initWithImage:self.images[i]];
-        CGFloat imageX = i * SCREEN_WIDTH;
-        imageView.frame = CGRectMake(imageX, 0, SCREEN_WIDTH, SCROLL_VIEW_H);
-        
+//    for (int i = 0; i < IMAGE_COUNT; ++i) {
+//        UIImageView* imageView = [[UIImageView alloc] initWithImage:self.images[i]];
+//        CGFloat imageX = i * SCREEN_WIDTH;
+//        imageView.frame = CGRectMake(imageX, 0, SCREEN_WIDTH, SCROLL_VIEW_H);
+//        
+//        imageView.contentMode = UIViewContentModeScaleAspectFit;
+//        [self.scrollView addSubview:imageView];
+//    }
+//    [self.view addSubview:self.scrollView];
+    
+    self.leftImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCROLL_VIEW_H)];
+    self.centerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(SCREEN_WIDTH, 0, SCREEN_WIDTH, SCROLL_VIEW_H)];
+    self.rightImageView = [[UIImageView alloc] initWithFrame:CGRectMake(SCREEN_WIDTH * 2, 0, SCREEN_WIDTH, SCROLL_VIEW_H)];
+    
+    for (UIImageView* imageView in @[self.leftImageView, self.centerImageView, self.rightImageView]) {
         imageView.contentMode = UIViewContentModeScaleAspectFit;
         [self.scrollView addSubview:imageView];
     }
-    [self.view addSubview:self.scrollView];
+}
+
+- (void) updateImageViews {
+    if (self.images.count == 0) return;
+    
+    NSInteger leftIndex = (self.currentIndex - 1 + self.images.count) % self.images.count;
+    NSInteger rightIndex = (self.currentIndex + 1) % self.images.count;
+    NSInteger currentIndex = self.currentIndex;
+    
+    self.leftImageView.image = self.images[leftIndex];
+    self.rightImageView.image = self.images[rightIndex];
+    self.centerImageView.image = self.images[currentIndex];
+    self.pageControl.currentPage = currentIndex;
 }
 
 - (void)setupPageControl {
@@ -79,11 +111,25 @@
     [self.view addSubview:self.pageControl];
 }
 
+//- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+//    CGFloat offsetX = scrollView.contentOffset.x;
+//    CGFloat pageWidth = scrollView.frame.size.width;
+//    int currentPage = round(offsetX / pageWidth);
+//    self.pageControl.currentPage = currentPage;
+//}
+
+
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     CGFloat offsetX = scrollView.contentOffset.x;
-    CGFloat pageWidth = scrollView.frame.size.width;
-    int currentPage = round(offsetX / pageWidth);
-    self.pageControl.currentPage = currentPage;
+    CGFloat width = scrollView.frame.size.width;
+    
+    if (offsetX >= 2 * width) {
+        self.currentIndex = (self.currentIndex + 1) % self.images.count;
+    } else if (offsetX <= 0) {
+        self.currentIndex = (self.currentIndex - 1 + self.images.count) % self.images.count;
+    }
+    
+    [self updateImageViews];
+    [scrollView setContentOffset:CGPointMake(width, 0)];
 }
-
 @end
